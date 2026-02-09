26c3f47f281cbff239fc0dfc145847edbb6386fe
Krish-vaghani <krishvaghani3800@gmail.com>
2026-02-09 15:44:48 +0530

    Update TestimonialCard styles to use rounded-full design for consistency

--- src/components/TestimonialsSection.tsx
+++ src/components/TestimonialsSection.tsx
@@ -80,7 +80,7 @@ const testimonialsRow2: Testimonial[] = [
 const TestimonialCard = ({ testimonial }: { testimonial: Testimonial }) => {
   return (
     <div
-      className="flex-shrink-0 w-[340px] sm:w-[400px] rounded-[40px] p-10 sm:p-12 select-none [&_*]:no-underline flex flex-col overflow-hidden"
+      className="flex-shrink-0 w-[340px] sm:w-[400px] rounded-full p-10 sm:p-12 select-none [&_*]:no-underline flex flex-col overflow-hidden"
       style={{
         backgroundImage: `url(${testimonialCardBg})`,
         backgroundSize: "cover",
