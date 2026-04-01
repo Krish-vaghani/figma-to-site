43ed5c07484fb6875426afbb7af29e6841bb96af
Krish-vaghani <krishvaghani3800@gmail.com>
2026-04-02 01:49:52 +0530

    dsqd

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,18 @@ import { Provider } from "react-redux";
 import App from "./App.tsx";
 import { store } from "./store";
 import "./index.css";
+import faviconUrl from "./assets/IMG_20260314_110118_242.webp";
+
+const favicon = document.createElement("link");
+favicon.rel = "icon";
+favicon.type = "image/webp";
+favicon.href = faviconUrl;
+document.head.appendChild(favicon);
+
+const appleTouch = document.createElement("link");
+appleTouch.rel = "apple-touch-icon";
+appleTouch.href = faviconUrl;
+document.head.appendChild(appleTouch);
 
 createRoot(document.getElementById("root")!).render(
   <Provider store={store}>
